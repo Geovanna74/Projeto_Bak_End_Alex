6bb7fde9418dce5353edcec035b349c00d0adca5
GEOVANNA CAROLINE CAMPOS LIMA <geovanna_c_lima@estudante.sesisenai.org.br>
2025-09-19 21:53:13 -0300

    aula pratica com alex

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import mysql from 'mysql2/promise'
+
+
+
+
+const pool = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senai',
+    database: 'api_node'
+});
+
+
+const app = express();
+app.get("/", (req,res)=>{
+    res.send("Olá Mundo")
+});
+
+app.get("/usuarios", async (req,res)=>{
+   const [results] = await pool.query(
+    'SELECT * FROM usuario'
+   );
+   res.send(results)
+});
+
+
+app.get("/usuarios/:id", async (req,res)=>{
+    const { id } = req.params
+    const [results] = await pool.query(
+     'SELECT * FROM usuario WHERE id_usuario=?', id
+    );
+    res.send(results)
+ });
+
+
+app.listen(3000, ()=>{
+console.log(`Servidor rodando na porta:3000`);
+});
