0cdc4e7672714782a4993b0f677807a8bb1281ed
GEOVANNA CAROLINE CAMPOS LIMA <geovanna_c_lima@estudante.sesisenai.org.br>
2025-09-26 22:07:03 -0300

    ATULIZAÇÃO DA AULA

--- src/app.js
+++ src/app.js
@@ -13,27 +13,67 @@ const pool = await mysql.createConnection({
 
 
 const app = express();
-app.get("/", (req,res)=>{
+app.use(express.json())
+
+app.get("/", (req, res) => {
     res.send("Olá Mundo")
 });
 
-app.get("/usuarios", async (req,res)=>{
-   const [results] = await pool.query(
-    'SELECT * FROM usuario'
-   );
-   res.send(results)
+app.get("/usuarios", async (req, res) => {
+    const [results] = await pool.query(
+        'SELECT * FROM usuario'
+    );
+    res.send(results)
 });
 
 
-app.get("/usuarios/:id", async (req,res)=>{
+app.get("/usuarios/:id", async (req, res) => {
     const { id } = req.params
     const [results] = await pool.query(
-     'SELECT * FROM usuario WHERE id_usuario=?', id
+        'SELECT * FROM usuario WHERE id_usuario=?', id
     );
     res.send(results)
- });
+});
 
 
-app.listen(3000, ()=>{
-console.log(`Servidor rodando na porta:3000`);
+app.post("/usuarios", async (req, res) => {
+    try {
+        const { body } = req
+        const [results] = await pool.query(
+            'INSERT INTO usuario (nome, idade) VALUES (?,?)',
+            [body.nome, body.idade]
+        );
+        const [usuarioCriado] = await pool.query("SELECT * FROM  usuario WHERE id_usuario=?", results.insertId)
+        return res.status(201).json(usuarioCriado)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+app.delete("/usuarios/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [results] = await pool.query("DELETE FROM usuario WHERE id_usuario=?", id);
+        res.status(200).send("Usuário deletado!", results)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { body } = req
+
+        const [results] = await pool.query("UPDATE usuario SET `nome` = ?, `idade` = ? WHERE id_usuario = ?;", [body.nome, body.idade, id])
+        res.status(200).send("Usuario atulizado", results)
+    } catch (error) {
+       console.log(error)
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log(`Servidor rodando na porta:3000`);
 });
